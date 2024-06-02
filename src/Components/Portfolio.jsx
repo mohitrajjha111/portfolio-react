e1ce9223dc2775b3ca63ca17acd0b34e5203c4bd
Mohit Raj Jha <125960660+mohitrajjha111@users.noreply.github.com>
2024-06-02 18:49:29 +0000

    Changed content

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -31,22 +31,22 @@ const imageAltText = "desktop with books and laptop";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
+    title: "How about checking your reaction speed? you know its fun🎉",
     description:
-      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
+      "A fun game to play with your friends built using javascipt to see who got the fastest fingers? who knows maybe you will need it to get a hotseat in KBC.",
+    url: "https://github.com/mohitrajjha111/reaction-tester",
   },
   {
-    title: "Web Development for Beginners",
+    title: "PGlife website",
     description:
-      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-    url: "https://github.com/microsoft/web-dev-for-beginners",
+      "A site that helps us easily find and select best PGs among many anytime from anywhere. Still in development phase. Learning and buildilng 😉",
+    url: "https://github.com/mohitrajjha111/pglife-website",
   },
   {
     title: "My Resume Site",
     description:
       "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
+    url: "https://github.com/mohitrajjha111/resume-website",
   },
   {
     title: "GitHub Codespaces and github.dev",
